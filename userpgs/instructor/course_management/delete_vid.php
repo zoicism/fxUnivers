73ea82d4a336cacda03cd2107ea68ddddbfc4080
Neo Abramson <neo@fxunivers.com>
2020-12-16 16:19:16 +0330

    edit course & del vid fixed

---
+++ userpgs/instructor/course_management/delete_vid.php
@@ -0,0 +1,21 @@
+<?php
+
+if(isset($_POST['course_id'])) {
+  $course_id=$_POST['course_id'];
+}
+
+
+$path='videos/';
+$file_ex=glob($path.$course_id.'.*');
+if(count($file_ex>0)) {
+  $vid_arr=explode('.', $file_ex[0]);
+  $vid_ext=end($vid_arr);
+  unlink('videos/'.$course_id.'.'.$vid_ext);
+
+  header('Location: /userpgs/instructor/course_management/course.php?course_id='.$course_id);
+} else {
+  header('Location: /userpgs/instructor/course_management/course.php?course_id='.$course_id);
+}
+
+
+?>
