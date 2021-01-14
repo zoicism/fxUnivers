04569facf62ab51fd350c621a983393f65092426
Neo Abramson <neo@fxunivers.com>
2021-01-14 20:12:48 +0330

    profile fxUniversity update

--- php/get_stu_stucourse.php
+++ php/get_stu_stucourse.php
@@ -1,13 +1,7 @@
 <?php
 
 $gss_query="SELECT * FROM stucourse WHERE stu_id=$get_user_id ORDER BY id DESC";
-$gss_result=mysqli_query($connection,$gss_query) or die(mysqli_error($connection));
-//$gss_fetch=mysqli_fetch_array($gss_result);
+$gss_result=mysqli_query($connection,$gss_query);
 $gss_count=mysqli_num_rows($gss_result);
-/*
-$first_gss_header=$gss_fetch['header'];
-$first_gss_desc=$gss_fetch['description'];
-$first_gss_courseId=$gss_fetch['course_id'];
-*/
 
 ?>
