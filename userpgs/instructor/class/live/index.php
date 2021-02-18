20b31163e2ccfa1a92974cfae3b602e34ffda416
Neo Abramson <neo@fxunivers.com>
2021-02-18 20:26:57 +0330

    toggles synced and buttons added

--- userpgs/instructor/class/live/index.php
+++ userpgs/instructor/class/live/index.php
@@ -86,11 +86,7 @@ require('../php/notify_students.php');
     <script src="../js/socket.io.js"> </script>
     <script src="../js/adapter-latest.js"></script>
     <script src="/js/webrtc/IceServersHandler.js"></script>
-<!--
-    <script src="../Pluginfree-Screen-Sharing/conference.js"></script>
-    <script src="../js/CodecsHandler.js"></script>
-    <script src="/js/webrtc/BandwidthHandler.js"></script>
--->
+
     <script src="/js/jquery-3.4.1.min.js"></script>
 
     <script src="/js/jquery.form.js"></script>
@@ -218,79 +214,52 @@ if($tar_user_fetch['avatar']!=NULL) {
 	  
 	</div>
 	<div class="right-content">
-	  <?php
-                require('../../../../php/limit_str.php');
-		?>
+	    <?php
+            require('../../../../php/limit_str.php');
+	    ?>
 
-<form id="wbForm" target="_blank" action="/userpgs/instructor/class/live/whiteboard/#<?php echo $class_id ?>" method="POST" style="display:none">
-          <input type="hidden" name="courseId" value="<?php echo $course_id ?>">
-          <input type="hidden" name="classId" value="<?php echo $class_id ?>">
-          <input type="submit" value="Open whiteboard">
-         </form>
+	    <form id="wbForm" target="_blank" action="/userpgs/instructor/class/live/whiteboard/#<?php echo $class_id ?>" method="POST" style="display:none">
+		<input type="hidden" name="courseId" value="<?php echo $course_id ?>">
+		<input type="hidden" name="classId" value="<?php echo $class_id ?>">
+		<input type="submit" value="Open whiteboard">
+            </form>
 
-<?php
+	    <?php
 
-if($user_type=='instructor') {
+	    if($user_type=='instructor') {
 
-			     echo '<div class="options session-options">';
+		echo '<div class="options session-options">';
 
-			     //echo '<h3 style="text-align:center">Broadcast</h3>';
-
-			     //echo '<div class="add-box" id="live-whiteboard-box">Live Whiteboard </div>';
-
-echo '<div class="add-box" style="margin-top:0"><input type="checkbox" class="toggle-btn" id="video-audio-b-toggle">Video & Audio Broadcast</div>';
-echo '<div class="add-box" style="margin-top:0"><input type="checkbox" class="toggle-btn" id="screen-b-toggle">Screen Broadcast</div>';
-echo '<div class="add-box" style="margin-top:0"><input type="checkbox" class="toggle-btn" id="audio-b-toggle">Audio Broadcast</div>';
-echo '<div class="add-box" style="margin-top:0"><input type="checkbox" class="toggle-btn" id="live-whiteboard-box">Whiteboard Broadcast</div>';
+		
+		echo '<div class="add-box" style="margin-top:0"><input type="checkbox" class="toggle-btn" id="video-audio-b-toggle">Video & Audio Broadcast</div>';
+		echo '<div class="add-box" style="margin-top:0"><input type="checkbox" class="toggle-btn" id="audio-b-toggle">Audio Broadcast</div>';
 
 
-//echo '<button id="share-screen" class="setup" style="display:none">Share Your Screen</button>';
+		echo '<div class="add-box" id="screen-b"><img src="/images/background/screen-sharing.svg" style="height:28px;width:28px;margin-right:8px;margin-left:19px;"> Screen Broadcast</div>';
+		echo '<div class="add-box" id="live-whiteboard-box"><img src="/images/background/whiteboard-sharing.svg" style="height:28px;width:28px;margin-right:8px;margin-left:19px;"> Whiteboard Broadcast</div>';
+		echo '<div class="add-box" id="record-btn"><img src="/images/background/record.png" style="height:28px;width:28px;margin-right:8px;margin-left:19px;"> Record</div>';
 
-//echo '<hr class="hr-tct">';
-
-
-
-//echo '<h3 style="text-align:center">Record</h3>';
-
-//echo '<div class="add-box" style="margin-top:0"><input type="checkbox" class="toggle-btn" id="video-audio-r-toggle">Video & Audio Record</div>';
-//echo '<div class="add-box" style="margin-top:0"><input type="checkbox" class="toggle-btn">Screen & Audio Record</div>';
-
-echo '<div class="add-box" id="record-btn"><svg viewBox="0 0 32 32">
-				          <path d="M16,11.5A4.5,4.5,0,1,1,11.5,16,4.5,4.5,0,0,1,16,11.5m0-2A6.5,6.5,0,1,0,22.5,16,6.5,6.5,0,0,0,16,9.5Z"></path>
-					  <path d="M21.5,2h.3a.7.7,0,0,1,.4.6l.4,3.5A3.1,3.1,0,0,0,23.5,8l.5.5a3.1,3.1,0,0,0,1.8.9l3.5.4a.7.7,0,0,1,.6.4.8.8,0,0,
-					  1-.1.8l-2.2,2.8a2.6,2.6,0,0,0-.7,1.8.9.9,0,0,1,0,.8,2.6,2.6,0,0,0,.7,1.8L29.8,21a.8.8,0,0,1,.1.8.7.7,0,0,1-.6.4l-3.5.4a3.1,
-					  3.1,0,0,0-1.8.9l-.5.5a3.1,3.1,0,0,0-.9,1.8l-.4,3.5a.7.7,0,0,1-.4.6h-.3l-.5-.2-2.8-2.2a2.8,2.8,0,0,0-1.7-.7h-1a2.8,2.8,0,0,
-					  0-1.7.7L11,29.8l-.5.2h-.3a.7.7,0,0,1-.4-.6l-.4-3.5A3.1,3.1,0,0,0,8.5,24L8,23.5a3.1,3.1,0,0,0-1.8-.9l-3.5-.4a.7.7,0,0,1-.6-.4.8.8,
-					  0,0,1,.1-.8l2.2-2.8A2.5,2.5,0,0,0,5,16.4v-.8a2.5,2.5,0,0,0-.6-1.8L2.2,11a.8.8,0,0,1-.1-.8.7.7,0,0,1,.6-.4l3.5-.4A3.1,3.1,0,0,0,8,
-					  8.5L8.5,8a3.1,3.1,0,0,0,.9-1.8l.4-3.5a.7.7,0,0,1,.4-.6h.3l.5.2,2.8,2.2a2.8,2.8,0,0,0,1.7.7h1a2.8,2.8,0,0,0,1.7-.7L21,2.2l.5-.2m-11-2L9.5.2h0A2.7,
-					  2.7,0,0,0,7.8,2.5L7.4,6a.8.8,0,0,1-.2.5l-.7.7L6,7.4l-3.5.4A2.7,2.7,0,0,0,.2,9.5h0a2.6,2.6,0,0,0,.4,2.7L2.9,15a1.3,1.3,0,0,1,.1.6V16H3v.4a1.3,1.3,0,0,
-					  1-.1.6L.6,19.8a2.6,2.6,0,0,0-.4,2.7h0a2.7,2.7,0,0,0,2.3,1.7l3.5.4.5.2a2.3,2.3,0,0,0,.7.7.8.8,0,0,1,.2.5l.4,3.5a2.7,2.7,0,0,0,1.7,2.3h0l1,.2a2.7,2.7,0,0,
-					  0,1.7-.6L15,29.1l.5-.2h1l.5.2,2.8,2.3a2.7,2.7,0,0,0,1.7.6l1-.2h0a2.7,2.7,0,0,0,1.7-2.3l.4-3.5a.8.8,0,0,1,.2-.5,2.3,2.3,0,0,0,.7-.7l.5-.2,3.5-.4a2.7,2.7,
-					  0,0,0,2.3-1.7h0a2.6,2.6,0,0,0-.4-2.7L29.1,17a1.4,1.4,0,0,1-.2-.6c.1-.1.1-.2.1-.4h0c0-.2,0-.3-.1-.4a1.4,1.4,0,0,1,.2-.6l2.3-2.8a2.6,2.6,0,0,0,.4-2.7h0a2.7,
-					  2.7,0,0,0-2.3-1.7L26,7.4l-.5-.2a2.3,2.3,0,0,0-.7-.7.8.8,0,0,1-.2-.5l-.4-3.5A2.7,2.7,0,0,0,22.5.2h0l-1-.2a2.7,2.7,0,0,0-1.7.6L17,2.9l-.5.2h-1L15,2.9,12.2.6A2.7,2.7,0,0,0,10.5,0Z"></path>
-			              </svg> Record</div>';
-
-			     echo '</div>';
-} else {
-echo '<!-- list of all available broadcasting rooms -->
+		echo '</div>';
+	    } else {
+		echo '<!-- list of all available broadcasting rooms -->
           <table style="display:none" id="rooms-list"></table>';
 
-			     echo '<div class="options session-options">';
+		echo '<div class="options session-options">';
 
-			     
+		
 
-			     echo '<div class="add-box" id="live-whiteboard-box" style="justify-content:center"><img src="/images/background/whiteboard-sharing.svg" style="height:28px;width:28px;margin-right:8px;"> Live Whiteboard</div>';
-			     echo '<div class="add-box" id="student-screen" style="justify-content:center"><img src="/images/background/screen-sharing.svg" style="height:28px;width:28px;margin-right:8px;"> Live Screen</div>';
-			     
+		echo '<div class="add-box" id="live-whiteboard-box" style="justify-content:center"><img src="/images/background/whiteboard-sharing.svg" style="height:28px;width:28px;margin-right:8px;"> Live Whiteboard</div>';
+		echo '<div class="add-box" id="student-screen" style="justify-content:center"><img src="/images/background/screen-sharing.svg" style="height:28px;width:28px;margin-right:8px;"> Live Screen</div>';
+		
 
 
-			     
+		
 
-			     echo '</div>';
-}
+		echo '</div>';
+	    }
 
-echo '<div class="sessions">';
-?>
+	    echo '<div class="sessions">';
+	    ?>
 
 
 <div class="tabs">
@@ -685,29 +654,28 @@ document.getElementById('setup-new-broadcast').onclick = function() {
 
 <!-- WHITEBOARD -->
 <script>
-$('#live-whiteboard-box').click( function() {
-$('#wbForm').submit();
-});
+ $('#live-whiteboard-box').click( function() {
+     $('#wbForm').submit();
+ });
 </script>
 
 <!-- ONLINE USERS -->
 <script>
-$(document).ready(function() {
-  setInterval(function() {
-    //console.log("<?php echo $tar_id?>");
-    jQuery.ajax({
-      url:'/php/live_online_users.php',
-      type:'POST',
-      data: {class_id:"<?php echo $class_id?>", username:"<?php echo $username?>", user_id:"<?php echo $get_user_id?>",course_id:"<?php echo $course_id?>", teacherId:"<?php echo $tar_id?>"},
-      success: function(response) {
-	$('.online-list').html(response);
-	$('#online-num').html($('#get-online-num').text());
-	//console.log(response);
-      }
-    });
-  }, 2000);
-});
-    
+ $(document).ready(function() {
+     setInterval(function() {
+	 //console.log("<?php echo $tar_id?>");
+	 jQuery.ajax({
+	     url:'/php/live_online_users.php',
+	     type:'POST',
+	     data: {class_id:"<?php echo $class_id?>", username:"<?php echo $username?>", user_id:"<?php echo $get_user_id?>",course_id:"<?php echo $course_id?>", teacherId:"<?php echo $tar_id?>"},
+	     success: function(response) {
+		 $('.online-list').html(response);
+		 $('#online-num').html($('#get-online-num').text());
+		 //console.log(response);
+	     }
+	 });
+     }, 2000);
+ });
 </script>
 
 <!-- FILES -->
@@ -838,66 +806,90 @@ $('#fileForm').ajaxForm(function(response) {
 </script>
 
 <script>
-$('#video-audio-b-toggle').change(function() {
-  if(this.checked) {
-    $('#broadcasting-option #audio-video-b').attr('selected','selected');
-    $('#setup-new-broadcast').click();
-  } else if(!this.checked) {
-      var streamingVid = document.getElementById('video-broadcast');
+ $('#video-audio-b-toggle').change(function() {
+     if(this.checked) {
+	 $('#broadcasting-option #audio-video-b').attr('selected','selected');
+	 $('#setup-new-broadcast').click();
+	 $('#audio-b-toggle').prop('disabled',true);
+	 $('#audio-b-toggle').css('opacity','0.6');
+     } else if(!this.checked) {
+	 var streamingVid = document.getElementById('video-broadcast');
+	 const stream = streamingVid.srcObject;
+	 const tracks = stream.getTracks();
+	 
+	 tracks.forEach(function(track) {
+	     track.stop();
+	 });
 
-      
-      const stream = streamingVid.srcObject;
-      const tracks = stream.getTracks();
+	 console.log('video stream stopped');
+	 $('.ins-vid-cnt #video-broadcast').remove();
 
-      tracks.forEach(function(track) {
-       track.stop();
-      });
-  
+	 window.location.reload();
+	 
+	 /*
+	 $('#audio-b-toggle').prop('disabled',false);
+	 $('#audio-b-toggle').css('opacity','1');
+	 */
+     }
+ });
+
+ $('#audio-b-toggle').change(function() {
+     if(this.checked) {
+	 $('#broadcasting-option #audio-b').attr('selected','selected');
+	 $('#setup-new-broadcast').click();
+
+	 $('#video-audio-b-toggle').prop('disabled',true);
+	 $('#video-audio-b-toggle').css('opacity','0.6');
+     } else if(!this.checked) {
+	 var streamingAudio = document.getElementById('live-session').querySelector('audio');
+	 const stream = streamingAudio.srcObject;
+	 const tracks = stream.getTracks();
+
+	 tracks.forEach(function(track) {
+	     track.stop();
+	 });
+
+	 $('.ins-vid-cnt #video-broadcast').remove();
+
+	 window.location.reload();
+	 
+	 /*
+	 $('#video-audio-b-toggle').prop('disabled',false);
+	 $('#video-audio-b-toggle').css('opacity','1');
+	  */
+     }
+ });
+
+ $('#screen-b').click(function() {
+     $('#live-screen').submit();
+ });
+
  
-    console.log('video stream stopped');
-    $('.ins-vid-cnt #video-broadcast').remove();
-  }
-});
 
-$('#audio-b-toggle').change(function() {
-  if(this.checked) {
-    $('#broadcasting-option #audio-b').attr('selected','selected');
-    $('#setup-new-broadcast').click();
-  } else if(!this.checked) {
-    var streamingAudio = document.getElementById('live-session').querySelector('audio');
-    const stream = streamingAudio.srcObject;
-    const tracks = stream.getTracks();
+ /*
+ $('#screen-b-toggle').change(function() {
+     if(this.checked) {
+	 $('#live-screen').submit();
+     } else if(!this.checked) {
+	 var streamingScr = document.getElementById('screen-broadcast');
 
-    tracks.forEach(function(track) {
-      track.stop();
-    });
-    $('.ins-vid-cnt #video-broadcast').remove();
-  }
-});
+	 const stream = streamingScr.srcObject;
+	 const tracks = stream.getTracks();
 
-$('#screen-b-toggle').change(function() {
-  if(this.checked) {
-    //$('#share-screen').click();
-    $('#live-screen').submit();
-  } else if(!this.checked) {
-      var streamingScr = document.getElementById('screen-broadcast');
+	 tracks.forEach(function(track) {
+	     track.stop();
+	 });
 
-      const stream = streamingScr.srcObject;
-      const tracks = stream.getTracks();
-
-      tracks.forEach(function(track) {
-       track.stop();
-      });
-
-    $('.ins-vid-cnt #screen-broadcast').remove();
-  }
-});
-
-$('#record-btn').click(function() {
-  var left = ($(window).width()/2)-(900/2);
-  var top = ($(window).width()/2)-(600/2);
-  window.open("/userpgs/instructor/class/RecordRTC/?classId="+<?php echo $class_id?>, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top="+top+",left="+left+",width=900,height=600"); 
-});
+	 $('.ins-vid-cnt #screen-broadcast').remove();
+     }
+ });
+ */
+ 
+ $('#record-btn').click(function() {
+     var left = ($(window).width()/2)-(900/2);
+     var top = ($(window).width()/2)-(600/2);
+     window.open("/userpgs/instructor/class/RecordRTC/?classId="+<?php echo $class_id?>, "_blank", "toolbar=yes,scrollbars=yes,resizable=yes,top="+top+",left="+left+",width=900,height=600"); 
+ });
 </script>
 
 
