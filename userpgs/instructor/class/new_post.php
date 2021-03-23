a025aa3116eeadd5941f219742c46bed7271c132
Neo Abramson <neo@fxunivers.com>
2021-03-23 13:51:17 +0430

    theTime added to db

--- userpgs/instructor/class/new_post.php
+++ userpgs/instructor/class/new_post.php
@@ -13,33 +13,36 @@ if(isset($_SESSION['username'])) {
 }
 
 
-    // if the values of the post is set, post it
-    if(isset($_POST['header'])) {
-	$header = $_POST['header'];
-	$header=mysqli_real_escape_string($connection,$header);
-    }
-    if(isset($_POST['description'])) {
-	$description = nl2br($_POST['description']);
-	$description=mysqli_real_escape_string($connection,$description);
-    }
-    if(isset($_POST['course_id'])) $course_id = $_POST['course_id'];
+// if the values of the post is set, post it
+if(isset($_POST['header'])) {
+    $header = $_POST['header'];
+    $header=mysqli_real_escape_string($connection,$header);
+}
+if(isset($_POST['description'])) {
+    $description = nl2br($_POST['description']);
+    $description=mysqli_real_escape_string($connection,$description);
+}
+if(isset($_POST['course_id'])) $course_id = $_POST['course_id'];
+
+$theTime = date('H:i');
+$theDate = date('Y-m-d');
 
 
-    $query = "INSERT INTO `class` (teacher_id, course_id, title, body, dt) VALUES ($id, $course_id, '$header', '$description', NOW())";
-    //echo '>>'.$query;
-    $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+$query = "INSERT INTO `class` (teacher_id, course_id, title, body, dt, theTime) VALUES ($id, $course_id, '$header', '$description', '$theDate', '$theTime')";
+//echo '>>'.$query;
+$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
 
-    $classId_query="SELECT * FROM class WHERE teacher_id=$id ORDER BY id DESC LIMIT 1";
-    $classId_result=mysqli_query($connection,$classId_query) or die(mysqli_error($connection));
-    $classId_fetch=mysqli_fetch_array($classId_result);
-    $classId=$classId_fetch['id'];
+$classId_query="SELECT * FROM class WHERE teacher_id=$id ORDER BY id DESC LIMIT 1";
+$classId_result=mysqli_query($connection,$classId_query) or die(mysqli_error($connection));
+$classId_fetch=mysqli_fetch_array($classId_result);
+$classId=$classId_fetch['id'];
 
 
-    if($result) {    
-	//echo "successful";
-	header("Location: /userpgs/instructor/class?course_id=$course_id&class_id=$classId");
-    } else {
-	//echo "failed";
-    }
+if($result) {    
+    //echo "successful";
+    header("Location: /userpgs/instructor/class?course_id=$course_id&class_id=$classId");
+} else {
+    //echo "failed";
+}
 
 ?>
