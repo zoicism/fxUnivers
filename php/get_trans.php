22f18f7dfdf8e74bcf38854da24c966f828eb452
Neo Abramson <neo@fxunivers.com>
2021-04-19 16:03:04 +0430

    updated getting txn to cst-time.

--- php/get_trans.php
+++ php/get_trans.php
@@ -1,7 +1,16 @@
 <?php
 
-require('../../wallet/php/wallet_connect.php');
+require($_SERVER['DOCUMENT_ROOT'].'/wallet/php/wallet_connect.php');
 
+$get_trans_in_q = "SELECT * FROM fxstar_txn WHERE from_id = $get_user_id ORDER BY id DESC";
+$get_trans_in = mysqli_query($wallet_connection, $get_trans_in_q);
+$get_trans_in_count=mysqli_num_rows($get_trans_in);
+
+$get_trans_out_q = "SELECT * FROM fxstar_txn WHERE to_id = $get_user_id ORDER BY id DESC";
+$get_trans_out=mysqli_query($wallet_connection, $get_trans_out_q);
+$get_trans_out_count=mysqli_num_rows($get_trans_out);
+
+/*
 $get_trans_in_q="SELECT * FROM transactions WHERE from_id=$get_user_id ORDER BY id DESC";
 $get_trans_in=mysqli_query($wallet_connection,$get_trans_in_q) or die(mysqli_error($wallet_connection));
 $get_trans_in_count=mysqli_num_rows($get_trans_in);
@@ -9,4 +18,5 @@ $get_trans_in_count=mysqli_num_rows($get_trans_in);
 $get_trans_out_q="SELECT * FROM transactions WHERE to_id=$get_user_id ORDER BY id DESC";
 $get_trans_out=mysqli_query($wallet_connection,$get_trans_out_q) or die(mysqli_error($wallet_connection));
 $get_trans_out_count=mysqli_num_rows($get_trans_out);
+*/
 ?>
