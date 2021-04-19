453cba11e561c4d9f1fc67499253141902b21b81
Neo Abramson <neo@fxunivers.com>
2021-04-19 16:15:55 +0430

    added init for fxStars in cst-time.

--- php/signup2.php
+++ php/signup2.php
@@ -13,10 +13,18 @@ $email = $_POST['email'];
 $email=mysqli_real_escape_string($connection,$email);
 $hash = $_POST['hash'];
 if(isset($_POST['phonenumber']) && !empty($_POST['phonenumber'])) {
-  $phonenumber = $_POST['phonenumber'];
+    $phonenumber = $_POST['phonenumber'];
 } else {
-  $phonenumber = 'NA';
+    $phonenumber = 'NA';
 }
+
+// Get ID of the user
+$get_id_q = "SELECT * FROM user WHERE email='$email'";
+$get_id_r = mysqli_query($connection, $get_id_q);
+$get_id_f = mysqli_fetch_array($get_id_r);
+$get_id = $get_id_f['id'];
+
+
 $uname_dup_query = "SELECT * FROM user WHERE username='$username'";
 $uname_dup_result = mysqli_query($connection, $uname_dup_query) or die(mysqli_error($connection));
 $uname_dup_num = mysqli_num_rows($uname_dup_result);
@@ -24,18 +32,23 @@ $uname_dup_num = mysqli_num_rows($uname_dup_result);
 $un_valid=1;
 
 if($uname_dup_num>0) {
-  $un_valid = 0;
-  header("Location: /register/verify_email.php?email=".$email."&hash=".$hash."&err=dup");
+    $un_valid = 0;
+    header("Location: /register/verify_email.php?email=".$email."&hash=".$hash."&err=dup");
 }
 
 if($un_valid) {
-   $signup2_query = "UPDATE user SET fname='$firstname', lname='$lastname', username='$username', phone='$phonenumber' WHERE email='$email'";
-   $signup2_result = mysqli_query($connection, $signup2_query) or die(mysqli_error($connection));
+    $signup2_query = "UPDATE user SET fname='$firstname', lname='$lastname', username='$username', phone='$phonenumber' WHERE email='$email'";
+    $signup2_result = mysqli_query($connection, $signup2_query) or die(mysqli_error($connection));
 
-  if($signup2_result) {
-    $_SESSION['username'] = $username;
-    header("Location: /userpgs");
-  }
+    // Set fxStars
+    require($_SERVER['DOCUMENT_ROOT'].'/wallet/php/wallet_connect.php');
+    $set_fxstars_q = "INSERT INTO fxstars(user_id) VALUES($get_id)";
+    $set_fxstars_r = mysqli_query($wallet_connection, $set_fxstars_q);
+    
+    if($signup2_result) {
+	$_SESSION['username'] = $username;
+	header("Location: /userpgs");
+    }
 
 }
 ?>
