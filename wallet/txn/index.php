79a7f358f06cbfe25496dc5da2a78f9e3960080f
Neo Abramson <neo@fxunivers.com>
2021-05-01 22:55:47 +0430

    switched incoming/outgoing places.

--- wallet/txn/index.php
+++ wallet/txn/index.php
@@ -93,74 +93,74 @@ require('../../php/get_trans.php');
   <div class="content-box" id="txn-content-box">
                           <h2>Transactions</h2>
                           
-<div class="row">
-                          <div class="col" style="overflow:hidden"><h3>Outgoing fxStars</h3></div>
-                          <div class="col" style="overflow:hidden"><h3>Incoming fxStars</h3></div>
-                          </div>
+<div class="row" style="width:100%">
+                          
+                          <div class="col" style="overflow:hidden;"><h3>Incoming</h3></div>
+			  <div class="col" style="overflow:hidden;"><h3>Outgoing</h3></div>
+</div>
 
 
 
 
                        
-              <div class="row">
+              <div class="row" style="width:100%">
                           
-                          <div class="col" style="border-right:1px solid gray">
-
-<?php
-          if($get_trans_in_count>0) {
-              $trans_i=1;
-              while($trans_in_row=$get_trans_in->fetch_assoc()) {
-
-	        if($trans_in_row['to_id']!=1) {
-		
-                  $trans_to_un_q='SELECT * FROM user WHERE id='.$trans_in_row['to_id'];
-                  $trans_to_un_r=mysqli_query($connection,$trans_to_un_q) or die(mysqli_error($connection));
-                  $trans_to_un_fetch=mysqli_fetch_array($trans_to_un_r);
-                  $trans_to_un=$trans_to_un_fetch['username'];
-
-                  echo '<div style="border-bottom:1px solid gray"><p>To: <strong><a href="/user/'.$trans_to_un.'">@'.$trans_to_un.'</a></strong></p> <p>Amount: <strong>'.$trans_in_row['amnt'].' fxStars (+'.ceil($trans_in_row['amnt']*0.1).' interest)</strong></p> <p>Date & Time: <strong>'.date("M jS, Y H:i", strtotime($trans_in_row['dt'])).' (UTC)</strong></p></div>';
-                  $trans_i++;
-		  
-		}
-              }
-          } else {
-              echo '<div><p style="color:gray">No outgoing transaction yet</p></div>';
-          }
-?>
-
-                          </div>
+                                                   
+                  <div class="col">
+		      <?php
+		      if($get_trans_out_count>0) {
+			  $trans_i=1;
+			  while($trans_out_row=$get_trans_out->fetch_assoc()) {
+			      $trans_from_un_q='SELECT * FROM user WHERE id='.$trans_out_row['from_id'];
+			      $trans_from_un_r=mysqli_query($connection,$trans_from_un_q) or die(mysqli_error($connection));
+			      $trans_from_un_fetch=mysqli_fetch_array($trans_from_un_r);
+			      $trans_from_un=$trans_from_un_fetch['username'];
+			      /*
+				 $trans_to_un_q='SELECT * FROM user WHERE id='.$trans_out_row['to_id'];
+				 $trans_to_un_r=mysqli_query($connection,$trans_to_un_q) or die(mysqli_error($connection));
+				 $trans_to_un_fetch=mysqli_fetch_array($trans_to_un_r);
+				 $trans_to_un=$trans_to_un_fetch['username'];
+			       */
+			      echo '<div style="border-bottom:1px solid gray"><p>From: <strong><a href="/user/'.$trans_from_un.'">@'.$trans_from_un.'</a></strong></p> <p>Amount: <strong>'.$trans_out_row['amnt'].' fxStars</strong></p><p>Date & Time: <strong>'.date("M jS, Y H:i", strtotime($trans_out_row['dt'])).' (UTC)</strong></p></div>';
+			      $trans_i++;
+			  }
+		      } else {
+			  echo '<div><p style="color:gray">No incoming transaction yet</p></div>';
+		      }
+		      ?>
 
 
 
+                  </div>
 
-                          
-                          <div class="col">
-<?php
-          if($get_trans_out_count>0) {
-              $trans_i=1;
-              while($trans_out_row=$get_trans_out->fetch_assoc()) {
-                  $trans_from_un_q='SELECT * FROM user WHERE id='.$trans_out_row['from_id'];
-                  $trans_from_un_r=mysqli_query($connection,$trans_from_un_q) or die(mysqli_error($connection));
-                  $trans_from_un_fetch=mysqli_fetch_array($trans_from_un_r);
-                  $trans_from_un=$trans_from_un_fetch['username'];
-/*
-                  $trans_to_un_q='SELECT * FROM user WHERE id='.$trans_out_row['to_id'];
-                  $trans_to_un_r=mysqli_query($connection,$trans_to_un_q) or die(mysqli_error($connection));
-                  $trans_to_un_fetch=mysqli_fetch_array($trans_to_un_r);
-                  $trans_to_un=$trans_to_un_fetch['username'];
-*/
-                  echo '<div style="border-bottom:1px solid gray"><p>From: <strong><a href="/user/'.$trans_from_un.'">@'.$trans_from_un.'</a></strong></p> <p>Amount: <strong>'.$trans_out_row['amnt'].' fxStars</strong></p><p>Date & Time: <strong>'.date("M jS, Y H:i", strtotime($trans_out_row['dt'])).' (UTC)</strong></p></div>';
-                  $trans_i++;
-              }
-          } else {
-              echo '<div><p style="color:gray">No incoming transaction yet</p></div>';
-          }
-?>
+		  <div class="col" style="border-right:1px solid gray">
+
+		      <?php
+		      if($get_trans_in_count>0) {
+			  $trans_i=1;
+			  while($trans_in_row=$get_trans_in->fetch_assoc()) {
+
+			      if($trans_in_row['to_id']!=1) {
+				  
+				  $trans_to_un_q='SELECT * FROM user WHERE id='.$trans_in_row['to_id'];
+				  $trans_to_un_r=mysqli_query($connection,$trans_to_un_q) or die(mysqli_error($connection));
+				  $trans_to_un_fetch=mysqli_fetch_array($trans_to_un_r);
+				  $trans_to_un=$trans_to_un_fetch['username'];
+
+				  echo '<div style="border-bottom:1px solid gray"><p>To: <strong><a href="/user/'.$trans_to_un.'">@'.$trans_to_un.'</a></strong></p> <p>Amount: <strong>'.$trans_in_row['amnt'].' fxStars (+'.ceil($trans_in_row['amnt']*0.1).' interest)</strong></p> <p>Date & Time: <strong>'.date("M jS, Y H:i", strtotime($trans_in_row['dt'])).' (UTC)</strong></p></div>';
+				  $trans_i++;
+				  
+			      }
+			  }
+		      } else {
+			  echo '<div><p style="color:gray">No outgoing transaction yet</p></div>';
+		      }
+		      ?>
+
+                  </div>
 
 
-
-                          </div>
-
+			  
               </div>
 
 
