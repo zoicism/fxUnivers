9ead6350311a01a035499916d97ad60c77cd6d92
Neo Abramson <neo@fxunivers.com>
2021-05-04 22:33:32 +0430

    added https requirement.

--- about/index.php
+++ about/index.php
@@ -1,3 +1,12 @@
+<?php
+// Requiring https
+if($_SERVER['HTTPS'] != "on") {
+    $url = "https://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+    header("Location: $url");
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
