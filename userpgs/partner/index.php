b122581b8321f00500399faf996c9ad9c89c45bd
10gician <neo@fxunivers.com>
2021-05-09 14:18:17 -0500

    added guidance.

--- userpgs/partner/index.php
+++ userpgs/partner/index.php
@@ -1,18 +1,18 @@
 <?php
-// Requiring https
-/*if($_SERVER['HTTPS'] != "on") {
-   $url = "https://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-   header("Location: $url");
-   exit;
-   }*/
+  // Requiring https
+  /*if($_SERVER['HTTPS'] != "on") {
+    $url = "https://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+    header("Location: $url");
+    exit;
+    }*/
 
 session_start();
 require('../../register/connect.php');
 
 if(isset($_SESSION['username'])) {
-    $username = $_SESSION['username'];
+  $username = $_SESSION['username'];
 } else {
-    require_once($_SERVER['DOCUMENT_ROOT'].'/php/get_login_cookies.php');
+  require_once($_SERVER['DOCUMENT_ROOT'].'/php/get_login_cookies.php');
 }
 
 require('../../php/get_user.php');
@@ -31,100 +31,108 @@ require('../../wallet/php/get_fxcoin_count.php');
 <!DOCTYPE html>
 <html>
 <head>
-	<title>fxPartner</title>
-    <meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/styles.css">
-    <link rel="stylesheet" href="/css/icons.css">
-    <link rel="stylesheet" href="/css/logo.css">
-    <script src="/js/jquery-3.4.1.min.js"></script>
-</head>
+<title>fxPartner</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/css/styles.css">
+  <link rel="stylesheet" href="/css/icons.css">
+  <link rel="stylesheet" href="/css/logo.css">
+  <script src="/js/jquery-3.4.1.min.js"></script>
+  </head>
     
-<body>
-	<div class="header-sidebar"></div>
-	<script id="upperbar-script" src="/js/upperbar.js" sess_avatar="<?php echo $session_avatar?>" sess_un="<?php echo $username?>"></script>
+  <body>
+  <div class="header-sidebar"></div>
+  <script id="upperbar-script" src="/js/upperbar.js" sess_avatar="<?php echo $session_avatar?>" sess_un="<?php echo $username?>"></script>
 	
-	<div class="blur mobile-main">
+  <div class="blur mobile-main">
     
-	<div class="sidebar"></div>
-	<?php require('../../php/sidebar.php'); ?>
+  <div class="sidebar"></div>
+  <?php require('../../php/sidebar.php'); ?>
 
 <div class="main-content">
 
-              <ul class="main-flex-container">
-                  <li class="main-items">
-                      <a href="/userpgs/partner/positions" class="link-main">
-                          <div class="head">fxHR</div>
-                      </a>
-                  </li>
-                  <li class="main-items">
-                      <a href="/userpgs/partner/income" class="link-main">
-                          <div class="head">Earnings History</div>
-                      </a>
-                  </li>
-                  
-              </ul>
+  <ul class="main-flex-container">
+  <li class="main-items">
+  <a href="/userpgs/partner/positions" class="link-main">
+  <div class="head">fxHR</div>
+  </a>
+<div class="extra-info-cnt" style="display:none">
+  <p class="extra-info">Copy and share the link provided here and we will share our interests with you.</p>
+  </div>
+  </li>
+  </li>
+  <li class="main-items">
+  <a href="/userpgs/partner/income" class="link-main">
+  <div class="head">Earnings History</div>
+  </a>
+  <div class="extra-info-cnt" style="display:none">
+  <p class="extra-info">History of the income you have made by sharing your fxHR link and building up our workforce</p>
+  </div>
 
-    </div>
+  </li>
+                  
+  </ul>
+
+  </div>
 
   <div class="relative-main-content">
-                            <div class="content-box">
-			    <?php
-                $total=0;
-                while($row=$get_partner_result->fetch_assoc()) {
-                    $total+=$row['income'];
-                }
+  <div class="content-box">
+  <?php
+  $total=0;
+while($row=$get_partner_result->fetch_assoc()) {
+  $total+=$row['income'];
+}
 ?>
-			    	 <h2>fxPartner</h2>
-                              	 <p>Earnings: <strong><?php echo $total ?> fxStars</strong></p>
-                            </div>
+<h2>fxPartner</h2>
+<p>Earnings: <strong><?php echo $total ?> fxStars</strong></p>
+</div>
 
 
 
 
 <div class="main-content-mob">
-<ul class="main-flex-container">
-                  <li class="main-items">
-                      <a href="/userpgs/partner/positions" class="link-main">
-                          <div class="head">fxHR</div>
-                      </a>
-                  </li>
-                  <li class="main-items">
-                      <a href="/userpgs/partner/income" class="link-main">
-                          <div class="head">Earnings History</div>
-                      </a>
-                  </li>
+  <ul class="main-flex-container">
+  <li class="main-items">
+  <a href="/userpgs/partner/positions" class="link-main">
+  <div class="head">fxHR</div>
+  </a>
+  
+  <li class="main-items">
+  <a href="/userpgs/partner/income" class="link-main">
+  <div class="head">Earnings History</div>
+  </a>
+  </li>
                   
-              </ul>
-</div>
+  </ul>
+  </div>
 
 
 
-			    <div class="description">
-			    <h3>fxPartner Options</h3>
-			    <p><strong>fxHR:</strong> Create an invitation link with which you can invite friends to use fxUnivers. We will share our profit from these new users with you half-half for 90 days.</p>
-			    <p><strong>Earnings History:</strong> The earnings you have made using fxHR.</p>
-    			    </div>
+  <div class="description">
+  <h3>fxPartner Options</h3>
+  <p><strong>fxHR:</strong> Create an invitation link with which you can invite friends to use fxUnivers. We will share our profit from these new users with you half-half for 90 days.</p>
+  <p><strong>Earnings History:</strong> The earnings you have made using fxHR.</p>
+  </div>
 
 
-    </div>
+  </div>
 
 
 
     
 
 
-</div>
+  </div>
 
 
-<div class="footbar blur"></div>
-                          <script src="/js/footbar.js"></script><script src="/js/notif_msg.js" id="notmsg" nmuid="<?php echo $get_user_id?>"></script>
+  <div class="footbar blur"></div>
+  <script src="/js/footbar.js"></script><script src="/js/notif_msg.js" id="notmsg" nmuid="<?php echo $get_user_id?>"></script>
 
 
 
-<!-- SCRIPTS -->
-<script>
-                          $('#page-header').html('fxPartner');
+  <!-- SCRIPTS -->
+  <script>
+  $('#page-header').html('fxPartner');
 $('#page-header').attr('href','/userpgs/partner/index.php');
 </script>
 
@@ -132,6 +140,14 @@ $('#page-header').attr('href','/userpgs/partner/index.php');
 <!-- fxPartner sidebar active -->
 <script>
 $('.fxpartner-sidebar').attr('id','sidebar-active');
+</script>
+
+<script>
+$('.main-items').hover(function() {
+    $(this).find('.extra-info-cnt').css('width',$(this).css('width')).show();
+  }, function() {
+    $(this).find('.extra-info-cnt').hide();
+  });
 </script>
 
 </body>
