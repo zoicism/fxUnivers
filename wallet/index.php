5e3deb0a939ed96969756cc4ca431d4589f3b432
Neo Abramson <neo@fxunivers.com>
2021-05-13 22:17:08 +0430

    added guidance and changed desc style.

--- wallet/index.php
+++ wallet/index.php
@@ -43,16 +43,29 @@ require('php/get_fxcoin_count.php');
     
 <body>
 	<div class="header-sidebar"></div>
-    <script id="upperbar-script" src="/js/upperbar.js" sess_avatar="<?php echo $session_avatar?>" sess_un="<?php echo $username?>"></script>
-
-<div class="blur mobile-main">
+	<script id="upperbar-script" src="/js/upperbar.js" sess_avatar="<?php echo $session_avatar?>" sess_un="<?php echo $username?>"></script>
+	<script>
+	 
+	 if(screen.width >= 629) {
+	     
+	     $(document).ready(function() {
+		 $('.header-sidebar').prepend('<div style="width:100%; display:flex; flex-flow:row nowrap; justify-content:left;"><div class="bar-items"><a href="/wallet/buy" class="link-main"><div class="head">Buy fxStars</div></a><div class="extra-info-cnt" style="display:none"><p class="extra-info">Buy fxStars and use them to enroll in courses.</p></div></div><div class="bar-items"><a href="/wallet/send" class="link-main"><div class="head">Transfer</div></a><div class="extra-info-cnt" style="display:none"><p class="extra-info">Send fxStars to friends securely and instantaneously.</p></div></div><div class="bar-items"><a href="/wallet/req" class="link-main"><div class="head">Request</div></a><div class="extra-info-cnt" style="display:none"><p class="extra-info">Request fxStars from friends and the transaction will take place as soon as they accept it.</p></div></div><div class="bar-items"><a href="/wallet/txn" class="link-main"><div class="head">Transactions</div></a><div class="extra-info-cnt" style="display:none"><p class="extra-info">History of fxStar transactions.</p></div></div><div class="bar-items"><a href="/wallet/cashout" class="link-main"><div class="head">Cash-out</div></a><div class="extra-info-cnt" style="display:none"><p class="extra-info">Request cash-out within 3-business-day delivery in USD, coming soon in all major cryptocurrencies.</p></div></div></div>');
+		 $('.bar-items').hover(function() {
+		     $(this).find('.extra-info-cnt').css('width',$(this).css('width')).show();
+		 }, function() {
+		     $(this).find('.extra-info-cnt').hide();
+		 });
+	     });
+	 }
+	</script>
+	<div class="blur mobile-main">
     
 	<div class="sidebar"></div>
 	<?php require('../php/sidebar.php'); ?>
 
                           
-    <div class="main-content">
-
+    <!--<div class="main-content">
+	
               <ul class="main-flex-container">
                   <li class="main-items">
                       <a href="/wallet/buy" class="link-main">
@@ -98,7 +111,7 @@ require('php/get_fxcoin_count.php');
                   </li>
               </ul>
 
-    </div>
+    </div>-->
 
 
 
@@ -106,8 +119,10 @@ require('php/get_fxcoin_count.php');
 
   <div class="relative-main-content">
                             <div class="content-box">
-                              <h2>fxStar</h2>
-                              <p>Balance: <strong><?php echo $get_fxcoin_count?> fxStars</strong></p>
+				<h2>fxStar Balance</h2>
+				<div class="icon-txt" style="opacity:1;font-size:1.6rem;">
+				    <p><div class="fxstar-price"></div><?php echo $get_fxcoin_count?></p>
+				</div>
                             </div>
 
 
@@ -152,18 +167,31 @@ require('php/get_fxcoin_count.php');
 
 
 
-                          <div class="description" style="font-size:0.6rem;">
-                          <h3>fxStar Options</h3>
-                          <p><strong>Buy fxStars:</strong> To use most parts of the fxUnivers you need fxStars and here is where you can buy some. 10 fxStars cost 11 USD.</p>
+                          <div class="description">
+			      <h3>What to use fxStars for?</h3>
+			      <p>You can use fxStars to:</p>
+			      <ul>
+				  <li>
+				      <p>Enroll in courses and make fxStars by creating fxSubCourses. <a href="/about#fxuniversity" >Learn more</a></p>
+				  </li>
+				  <li>
+				      <p>Send fxStars to your friends securely and instantaneously.</p>
+				  </li>
+			      </ul>
+                          <h3>How to Buy fxStars</h3>
+                          <p>Click on the <a href="/wallet/buy" ><strong>Buy fxStars</strong></a> above to buy fxStars. The cost of 10 fxStars is 11 USD.</p>
 
-                          <p><strong>Cash-out fxStars:</strong> With a minimum number of 100 fxStars you can request cash-outs which may take place within 3 business days. Coming soon, you will also be able to request cash-outs with various cryptocurrencies.</p>
-                          
+			  <h3>How to Transfer fxStars</h3>
+			  <p>Click on the <a href="/wallet/send" ><strong>Transfer</strong></a> above, enter how many fxStars and to whom you wish to send, and your friends will receive the fxStars instantly and securely! They will be informed of the transaction right away.</p>
 
-                          <p><strong>Transactions:</strong> The history of your fxStar transactions.</p>
+			  <h3>How to Request fxStars</h3>
+			  <p>Click on the <strong><a href="/wallet/req" >Request</a></strong> above and request a specific number of fxStars from any of your friends. They will get informed immediately and will send you the requested amount if they want to, with just one click.</p>
 
-                          <p><strong>Send fxStars:</strong> Send your friends fxStars instantly and securely! They will get the fxStars and be informed of the transaction right away.</p>
+			  <h3>How to see the Transactions</h3>
+			  <p>Click on the <strong><a href="/wallet/txn" >Transactions</a></strong> above to get a list of all your incoming and outgoing transactions.</p>
 
-                          <p><strong>Request fxStars:</strong> Request a specific number of fxStars from friends! They will get informed immediately and may send you the requested ammount with one click.</p>
+			  <h3>How to Cash-out</h3>
+			  <p>Click on the <strong><a href="/wallet/cashout" >Cash-out</a></strong> and submit a cash-out request of at least 100 fxStars which will take place within 3 business days. Coming soon, you will also be able to request instantaneous cash-outs with various cryptocurrencies.</p>                     
                         </div>
   </div>
 
@@ -197,11 +225,8 @@ $('#page-header').attr('href','/wallet');
 </script>
 
 <script>
- $('.main-items').hover(function() {
-     $(this).find('.extra-info-cnt').css('width',$(this).css('width')).show();
- }, function() {
-     $(this).find('.extra-info-cnt').hide();
- });
+ 
+ 
 </script>
 </body>
 </html>
