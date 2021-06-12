356a133134b99edd4ef4042c365eede9585e4046
10gician <neo@fxunivers.com>
2021-06-12 14:08:35 +0430

    fixed file-size issue.

--- php/sync_msgs.php
+++ php/sync_msgs.php
@@ -5,6 +5,25 @@ require_once('../contact/message_connect.php');
 if(isset($_POST['user_id'])) $get_user_id=$_POST['user_id'];
 if(isset($_POST['guest_id'])) $guest_id=$_POST['guest_id'];
 
+function formatSize($bytes) {
+	if($bytes >= 1073741824) {
+		$bytes = number_format($bytes/1073741824, 2). ' GB';
+	} elseif ($bytes >= 1048576) {
+            $bytes = number_format($bytes / 1048576, 2) . ' MB';
+        } elseif ($bytes >= 1024) {
+            $bytes = number_format($bytes / 1024, 2) . ' KB';
+        } elseif ($bytes > 1) {
+            $bytes = $bytes . ' bytes';
+        } elseif ($bytes == 1) {
+            $bytes = $bytes . ' byte';
+        } else {
+            $bytes = '0 bytes';
+        }
+
+	return $bytes;
+}
+
+
 $sync_msgs_q = "SELECT * FROM messenger WHERE (user2id=$get_user_id AND user1id=$guest_id AND read_dt IS NULL)";
 $sync_msgs_r = mysqli_query($msg_connection,$sync_msgs_q);
 $sync_msgs_count = mysqli_num_rows($sync_msgs_r);
@@ -24,7 +43,7 @@ if($sync_msgs_count>0) {
                   	      <div class="upload-recieved">
                     	        <img src="/images/background/file-recieved.svg">
                   	      </div>
-                  	      <div class="file-name">'.$sync_fetch['text'].'<div>'.$sync_fetch['file_size'].' MB</div></div>
+                  	      <div class="file-name">'.$sync_fetch['text'].'<div>'.$formatSize($sync_fetch['file_size']).'</div></div>
                   	      <span class="time">'.$sent_time->format('H:i').'</span>
                 	    </div>
        ';
