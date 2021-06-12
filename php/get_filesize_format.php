cac9a3096ce7ec0257848becedec9cc880be72ae
10gician <neo@fxunivers.com>
2021-06-12 14:59:18 +0430

    synced all file size formatting.

--- php/get_filesize_format.php
+++ php/get_filesize_format.php
@@ -1,15 +1,13 @@
 <?php
 function formatSize($bytes) {
-	if($bytes >= 1073741824) {
-		$bytes = number_format($bytes/1073741824, 2). ' GB';
-	} elseif($bytes >= 1048576) {
-		$bytes = number_format($bytes/1048576, 2). ' MB';
+	if($bytes >= 1048576) {
+		$bytes = number_format($bytes/1048576, 2). ' GB';
 	} elseif($bytes >= 1024) {
-		$bytes = number_foormat($bytes / 1024, 2). ' KB';
+		$bytes = number_foormat($bytes / 1024, 2). ' MB';
 	} elseif($bytes > 1) {
-		$bytes = $bytes . ' bytes';
+		$bytes = $bytes . ' KB';
 	} elseif($bytes == 1) {
-		$bytes = $bytes . ' byte';
+		$bytes = $bytes . ' KB';
 	} else {
 		$bytes = '0 bytes';
 	}
--- php/sync_msgs.php
+++ php/sync_msgs.php
@@ -4,25 +4,7 @@ require_once('../contact/message_connect.php');
 
 if(isset($_POST['user_id'])) $get_user_id=$_POST['user_id'];
 if(isset($_POST['guest_id'])) $guest_id=$_POST['guest_id'];
-
-function formatSize($bytes) {
-	if($bytes >= 1073741824) {
-		$bytes = number_format($bytes/1073741824, 2). ' GB';
-	} elseif ($bytes >= 1048576) {
-            $bytes = number_format($bytes / 1048576, 2) . ' MB';
-        } elseif ($bytes >= 1024) {
-            $bytes = number_format($bytes / 1024, 2) . ' KB';
-        } elseif ($bytes > 1) {
-            $bytes = $bytes . ' bytes';
-        } elseif ($bytes == 1) {
-            $bytes = $bytes . ' byte';
-        } else {
-            $bytes = '0 bytes';
-        }
-
-	return $bytes;
-}
-
+require_once($_SERVER['DOCUMENT_ROOT'].'/php/get_filesize_format.php';
 
 $sync_msgs_q = "SELECT * FROM messenger WHERE (user2id=$get_user_id AND user1id=$guest_id AND read_dt IS NULL)";
 $sync_msgs_r = mysqli_query($msg_connection,$sync_msgs_q);
